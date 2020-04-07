39a22635745c2d79a95a61ca2e78f1c03b4e74d7
Aditya Azad <adityaazad121@gmail.com>
2020-04-06 23:29:11 -0400

    Users now cannot view the profile page before loggin in

--- client/src/components/pages/Profile.js
+++ client/src/components/pages/Profile.js
@@ -1,17 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { PageContainer, CenterContainer } from '../styleComponents';
 import Layout from "../Layout";
 
-const Profile = () => {
-  return (
-    <Layout isHome={false}>
-      <PageContainer>
-        <CenterContainer>
-          <h1>Profile</h1>
-        </CenterContainer>
-      </PageContainer>
-    </Layout>
-  );
+class Profile extends Component {
+
+  checkLogin = () => {
+    if (!this.props.auth) return true;
+    return false;
+  }
+
+  render() {
+    return (
+      <>
+      {this.checkLogin() ? <p>You need to login</p> :
+      <Layout isHome={false}>
+        <PageContainer>
+          <CenterContainer>
+            <h1>Profile</h1>
+          </CenterContainer>
+        </PageContainer>
+      </Layout>
+      }
+      </>
+    );
+  }
 };
 
-export default Profile;
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+
+export default connect(mapStateToProps)(Profile);
